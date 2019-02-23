4447ad7167838d3d0ca27dd33c348104c8e41c70
greybutton <greybutton@yandex.com>
2019-02-23 21:42:36 +0800

    create, delete tabs

--- __tests__/Tabs.test.jsx
+++ __tests__/Tabs.test.jsx
@@ -1,4 +1,7 @@
+import 'react-log-state';
 import App from '../src/components/Main';
+
+ReactLogState.logAll(); // eslint-disable-line
 
 const datatest = 'li[data-test^="tab"]';
 
@@ -43,5 +46,40 @@ describe('Tabs without snapshots', () => {
     const tab = wrapper.find(datatest).at(2);
     tab.simulate('click');
     expect(wrapper).toIncludeText('Any content 3');
+  });
+});
+
+describe('Tabs CRUD', () => {
+  test('create', () => {
+    const wrapper = mount(<App />);
+    const addButton = wrapper.find('[data-test="button-add"]');
+    addButton.simulate('click');
+    expect(wrapper).toContainMatchingElement('[data-test="form"]');
+    const inputTitle = wrapper.find('[data-test="input-title"]');
+    const inputContent = wrapper.find('[data-test="input-content"]');
+    const saveButton = wrapper.find('[data-test="button-submit"]');
+    const title = 'Title Test';
+    const content = 'Content Test';
+    inputTitle.simulate('change', { target: { value: title } });
+    inputContent.simulate('change', { target: { value: content } });
+    saveButton.simulate('click');
+    expect(wrapper).toIncludeText(content);
+  });
+
+  test('create cancel', () => {
+    const wrapper = mount(<App />);
+    const addButton = wrapper.find('[data-test="button-add"]');
+    addButton.simulate('click');
+    expect(wrapper).toContainMatchingElement('[data-test="form"]');
+    const cancelButton = wrapper.find('[data-test="button-cancel"]');
+    cancelButton.simulate('click');
+    expect(wrapper).not.toContainMatchingElement('[data-test="form"]');
+  });
+
+  test('delete', () => {
+    const wrapper = mount(<App />);
+    const removeButtons = wrapper.find('[data-test="button-remove"]');
+    removeButtons.at(2).simulate('click');
+    expect(wrapper).not.toIncludeText('Title 3');
   });
 });
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,27 +5,155 @@ import {
 import 'react-tabs/style/react-tabs.css';
 
 
-const Main = () => (
-  <div className="container">
-    <h1>Hello</h1>
-    <Tabs>
-      <TabList>
-        <Tab data-test="tab-0">Title 1</Tab>
-        <Tab disabled data-test="tab-1">Title 2 (disabled)</Tab>
-        <Tab data-test="tab-2">Title 3</Tab>
-      </TabList>
+class Main extends React.Component {
+  static id = 3;
 
-      <TabPanel>
-        <h2>Any content 1</h2>
-      </TabPanel>
-      <TabPanel>
-        <h2>Any content 2</h2>
-      </TabPanel>
-      <TabPanel>
-        <h2>Any content 3</h2>
-      </TabPanel>
-    </Tabs>
-  </div>
-);
+  state = {
+    tabIndex: 0,
+    tabs: [
+      { id: 0, title: 'Title 1', content: 'Any content 1' },
+      {
+        id: 1, title: 'Title 2 (disabled)', content: 'Any content 2', attrs: { disabled: true },
+      },
+      { id: 2, title: 'Title 3', content: 'Any content 3' },
+    ],
+    form: {
+      state: 'close',
+      title: '',
+      content: '',
+    },
+  }
+
+  handleSelectTab = tabIndex => this.setState({ tabIndex });
+
+  handleOpenNewForm = () => this.setState(prevState => ({ ...prevState, form: { state: 'new' } }));
+
+  handleCloseForm = () => this.setState(prevState => ({ ...prevState, form: { state: 'close' } }));
+
+  onChangeTitle = (e) => {
+    const { target: { value } } = e;
+    this.setState(prevState => ({ ...prevState, form: { ...prevState.form, title: value } }));
+  }
+
+  onChangeContent = (e) => {
+    const { target: { value } } = e;
+    this.setState(prevState => ({ ...prevState, form: { ...prevState.form, content: value } }));
+  }
+
+  handleRemoveTab = (id) => {
+    const { tabs } = this.state;
+    const newTabs = tabs.filter(t => t.id !== id);
+    this.setState(prevState => ({ ...prevState, tabs: newTabs }));
+  }
+
+  handleSaveTab = (e) => {
+    e.preventDefault();
+    const { form, tabs } = this.state;
+    const tab = {
+      id: Main.id,
+      title: form.title,
+      content: form.content,
+    };
+    const resetForm = {
+      state: 'close',
+      title: '',
+      content: '',
+    };
+    const newTabs = [...tabs, tab];
+    const activeTab = newTabs.length - 1;
+    this.setState({ tabs: newTabs, form: resetForm, tabIndex: activeTab });
+    Main.id += 1;
+  }
+
+  renderForm = () => (
+    <form
+      data-test="form"
+    >
+      <div className="form-group">
+        <label htmlFor="title">
+          Title
+          <input
+            data-test="input-title"
+            onChange={this.onChangeTitle}
+            type="text"
+            className="form-control"
+            id="title"
+            name="title"
+            aria-describedby="titleHelp"
+            placeholder="Enter title"
+          />
+        </label>
+      </div>
+      <div className="form-group">
+        <label htmlFor="content">
+          Content
+          <textarea
+            data-test="input-content"
+            onChange={this.onChangeContent}
+            className="form-control"
+            id="content"
+            name="content"
+            rows="3"
+          />
+        </label>
+      </div>
+      <button
+        data-test="button-submit"
+        onClick={this.handleSaveTab}
+        type="submit"
+        className="btn btn-primary"
+      >
+        Submit
+      </button>
+      <button
+        data-test="button-cancel"
+        onClick={this.handleCloseForm}
+        type="button"
+        className="btn btn-secondary"
+      >
+        Cancel
+      </button>
+    </form>
+  )
+
+  render() {
+    const { tabIndex, tabs, form } = this.state;
+    const isFormOpen = form.state === 'new';
+
+    return (
+      <div className="container">
+        <h1>Hello</h1>
+        <button
+          data-test="button-add"
+          type="button"
+          onClick={this.handleOpenNewForm}
+          className="btn btn-primary"
+        >
+          Add tab
+        </button>
+        {isFormOpen && this.renderForm()}
+        <Tabs selectedIndex={tabIndex} onSelect={this.handleSelectTab}>
+          <TabList>
+            {tabs.map(({ id, title, attrs }) => (
+              <Tab key={id} data-test="tab" {...attrs}>
+                {title}
+                {' '}
+                <button
+                  data-test="button-remove"
+                  type="button"
+                  onClick={() => this.handleRemoveTab(id)}
+                  className="btn btn-outline-secondary btn-sm"
+                >
+                  x
+                </button>
+              </Tab>
+            ))}
+          </TabList>
+          {tabs.map(({ id, content }) => <TabPanel key={id}>{content}</TabPanel>)}
+        </Tabs>
+      </div>
+    );
+  }
+}
 
 export default Main;
