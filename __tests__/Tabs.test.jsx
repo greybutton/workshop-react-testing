5574736a3197a175577dcf4ffe644e54087356e9
greybutton <greybutton@yandex.com>
2019-02-23 18:17:40 +0800

    add test tabs without snapshots

--- __tests__/Tabs.test.jsx
+++ __tests__/Tabs.test.jsx
@@ -1,8 +1,8 @@
 import App from '../src/components/Main';
 
-describe('Tabs', () => {
-  const datatest = 'li[data-test^="tab"]';
+const datatest = 'li[data-test^="tab"]';
 
+describe('Tabs', () => {
   test('render', () => {
     const wrapper = mount(<App />);
     expect(wrapper.render()).toMatchSnapshot();
@@ -20,5 +20,28 @@ describe('Tabs', () => {
     const tab = wrapper.find(datatest).at(2);
     tab.simulate('click');
     expect(wrapper.render()).toMatchSnapshot();
+  });
+});
+
+describe('Tabs without snapshots', () => {
+  test('first render', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper).toIncludeText('Any content 1');
+  });
+
+  test('click on disabled tab', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper).toIncludeText('Any content 1');
+    const tabDisabled = wrapper.find(datatest).at(1);
+    tabDisabled.simulate('click');
+    expect(wrapper).toIncludeText('Any content 1');
+  });
+
+  test('click on normal tab', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper).toIncludeText('Any content 1');
+    const tab = wrapper.find(datatest).at(2);
+    tab.simulate('click');
+    expect(wrapper).toIncludeText('Any content 3');
   });
 });
