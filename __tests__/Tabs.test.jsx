1b712444f6184863d7056ac8c38ff582670d4e38
greybutton <greybutton@yandex.com>
2019-02-23 17:35:00 +0800

    refactor tests to test whole app, not just tabs

--- __tests__/Tabs.test.jsx
+++ __tests__/Tabs.test.jsx
@@ -1,22 +1,22 @@
-import MyTabs from '../src/components/Tabs';
+import App from '../src/components/Main';
 
-describe('<Tabs />', () => {
+describe('Tabs', () => {
   const datatest = 'li[data-test^="tab"]';
 
   test('render', () => {
-    const wrapper = mount(<MyTabs />);
+    const wrapper = mount(<App />);
     expect(wrapper.render()).toMatchSnapshot();
   });
 
-  test('click on disabled', () => {
-    const wrapper = mount(<MyTabs />);
+  test('click on disabled tab', () => {
+    const wrapper = mount(<App />);
     const tabDisabled = wrapper.find(datatest).at(1);
     tabDisabled.simulate('click');
     expect(wrapper.render()).toMatchSnapshot();
   });
 
-  test('click on active tab', () => {
-    const wrapper = mount(<MyTabs />);
+  test('click on normal tab', () => {
+    const wrapper = mount(<App />);
     const tab = wrapper.find(datatest).at(2);
     tab.simulate('click');
     expect(wrapper.render()).toMatchSnapshot();
